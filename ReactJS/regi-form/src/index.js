9ed866111cf6bbf29927c6c1fee9033815a2cfec
hemzG <112705597+hemzG@users.noreply.github.com>
2022-09-22 19:10:47 +0530

    validation of react form

--- ReactJS/regi-form/src/index.js
+++ ReactJS/regi-form/src/index.js
@@ -29,37 +29,74 @@ class NameForm extends React.Component {
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
+    var formName=this.state.firstName;
+    var formEmail= this.state.email;
+    var formPhone =this.state.phoneNumber;
+    var formMessage=this.state.message;
+    var valEmail= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+
+    if(formName == undefined){
+      document.getElementById('name-error').innerHTML = "Please enter name";
+    }else{
+      document.getElementById('name-error').innerHTML = "";
+      }
+
+      if (formEmail == undefined || !valEmail.test(formEmail)) {
+        document.getElementById('email-error').innerHTML = "Please enter correct email id";
+  }
+  else{
+  document.getElementById('email-error').innerHTML = "";
+  }
+  
+  if(formPhone == undefined){
+    document.getElementById('phone-error').innerHTML = "Please enter phone number";
+  }else{
+    document.getElementById('phone-error').innerHTML = "";
+    }
+    if(formMessage == undefined){
+      document.getElementById('message-error').innerHTML = "Please enter message";
+    }else{
+      document.getElementById('message-error').innerHTML = "";
+      }
+
+
+
+   //alert('A name was submitted: ' + this.state.value);
+   event.preventDefault();
   }
   render() {
     return (
       <div>
       <div className="form">
-      <form onSubmit={this.handleSubmit}>
+      <form id = "valform" onSubmit={this.handleSubmit}>
         <div className="form-body">
         <div>
         <label className='form_label'>Name:
           <input type="text" className="form_input" name= "firstName" value={this.state.firstName} onChange={this.handleInputChange} placeholder="Name" />
         </label>
+        <div id="name-error" className ="errorclass"></div>
         </div>
         <div>
           <label className='form_label'>Email:
-          <input type="email" className="form_input" name= "email" value={this.state.email} onChange={this.handleInputChange} /> </label>
+          <input type="text" className="form_input" name= "email" value={this.state.email} onChange={this.handleInputChange} /> </label>
+          <div id="email-error"  className ="errorclass"></div>
           </div>
         <div>
         <label className='form_label'>Phone Number:
-          <input type="text" className="form_input" name= "phoneNumber" value={this.state.phoneNumber} onChange={this.handleInputChange}/>
+          <input type="number" className="form_input" name= "phoneNumber" value={this.state.phoneNumber} onChange={this.handleInputChange}/>
         </label>
+        <div id="phone-error"  className ="errorclass"></div>
         </div>
         <div>
         <label className='form_label'>Message:
           <textarea className="form_input" name= "message" value={this.state.message} onChange={this.handleInputChange} />
         </label>
+        <div id="message-error"  className ="errorclass"></div>
         </div>
         </div>
         <div className="footer">
-              <button type="submit" className="btn">Submit</button>
+          <input type="submit" value ="submit"></input>
+           
           </div>
       </form>
       </div>
